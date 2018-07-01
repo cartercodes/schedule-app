27538483cd7d51150c2a96cde7854cb9055fa343
Mitchell Carter <carter.svo@gmail.com>
2018-07-01 16:27:12 -0400

    small add in

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -4,7 +4,7 @@ class Library extends Component {
     render() {
         return (
             <div className="library">
-                <h1 className="library__title">My Schedule</h1>
+                <h1 className="library__title">Course Library</h1>
             </div>
         )
     }
