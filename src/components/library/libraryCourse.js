6007a7551e4c9c1be57f1c66fd7f5daab1ca36c0
Mitchell Carter <carter.svo@gmail.com>
2018-07-05 12:13:29 -0400

    integrated font awesome and built icon component

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
+import Icon from "../icon";
+
 class LibraryCourse extends Component {
     render() {
         return (
             <div className="library-course">
                 <label className="library-course__title">Problem Solving</label>
-                {/*icon component */}
+                {Icon ("fas fa-check", "library-course__icon") }
                 {/*aarow component */}
-                {/*action button component */}
                 <div className="library-course__description">
                     <label>Course Description</label>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet arcu urna, lobortis congue diam consectetur nec. Duis sit amet eleifend nulla.</p>
